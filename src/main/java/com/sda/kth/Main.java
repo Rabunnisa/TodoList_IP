241d8108371537990d2c06bac8e3f9b1f2a47a2c
Rabunnisa <rabia.zabiulla@gmail.com>
2021-03-10 23:12:10 +0100

    Comments start

--- src/main/java/com/sda/kth/Main.java
+++ src/main/java/com/sda/kth/Main.java
@@ -1,6 +1,12 @@
 
 package com.sda.kth;
 
+/**
+ * This is a main method which is used to run the code it has 3 methods important methods used to run the program .
+ * @readFile this method is used to read the data from file in this case used to read the task list which is already saved.
+ * @CheckDone this method is used to check whether the task is done or pending .
+ * @initSwitch this is used to allow user to choose between different options and display all the switch statements .
+ */
 public class Main {
 
 	public static void main(String[] args) {
