8f12729892bd9da39cc6dd99d087608569626732
Rabunnisa <rabia.zabiulla@gmail.com>
2021-03-12 17:50:41 +0100

    started javadoc

--- src/main/java/com/sda/kth/Main.java
+++ src/main/java/com/sda/kth/Main.java
@@ -1,6 +1,11 @@
 
 package com.sda.kth;
 
+/**
+ * This is a main method which triggers the execution of ToDoList App.
+ * it is loading tasks from the file by calling readFile().
+ * @initSwitch is used to provide the user to choose between different options .
+ */
 public class Main {
 	public static String constantString = "";
 
